aea1a83594034655beb814d655c449846b03ed86
Kuldeep Jambhulkar <89466207+kuldeepjambhulkar@users.noreply.github.com>
2022-09-22 21:09:06 +0530

    Clean-up

--- index.js
+++
@@ -1,6 +0,0 @@
-function camelIt (string) {
-    const preString = string.split(' ').map( item => item.charAt(0).toUpperCase() + item.slice(1)).join('')
-    return(preString.charAt(0).toLocaleLowerCase() + preString.slice(1))
-}
-
-module.exports = camelIt;
